b1ef7c2d609b91140abb3be340adde4a990c84c2
Alejandro Cueto <s3813718@students.fscj.edu>
2026-06-24 13:58:35 -0400

    Update PalmerPenguinsM7.java

--- PalmerPenguinsM7.java
+++ PalmerPenguinsM7.java
@@ -1,15 +1,15 @@
 // PalmerPenguinsM7.java
-//
-//
+// Alejandro Cueto
+// 6/24/26
 // Refactored Palmer Penguins analysis using object-oriented approach
 
 public class PalmerPenguinsM7 {
     public static void main(String[] args) {
         // Create an instance of SpeciesCounter
-        
+        SpeciesCounter counter = new SpeciesCounter();
 
         // Read species data
-        
+        counter.readSpeciesData();
 
         // Initialize species count
         counter.initializeSpeciesCount();
